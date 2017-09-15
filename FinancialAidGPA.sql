fc047489cf33a20d5472ff6842eb53354710157c
osuhomebase <mbaron999@gmail.com>
2017-09-14 23:07:37 -0400

    updated FinancialAidGPA.sql to look at max term rather than 1178

--- FinancialAidGPA.sql
+++ FinancialAidGPA.sql
@@ -1,5 +1,6 @@
 
 select sum(item_amt), ne.emplID, CUM_GPA from nyu.item_sf sf inner join nyuEnrollment ne on sf.emplID = ne.EmplID 
 inner join NYUAdmissionsImport na on ne.emplId = na.EmplID
-where ne.STRM = '1178' and na.Admit_Term = '1138' and na.Admit_Type = 'NFR' and cum_gpa <> 0
+where ne.STRM = (select max(ne2.STRM) from nyuEnrollment ne2 where ne2.emplID = ne.EmplID) and na.Admit_Term = '1138' and na.Admit_Type = 'NFR' and cum_gpa <> 0
 group by ne.EmplID, CUM_GPA
+
